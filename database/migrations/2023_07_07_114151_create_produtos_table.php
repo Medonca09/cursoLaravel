10b6c07fbcba4f60e1a82229bb6e898f1553d9fa
Lucas Mendonça <lucasmm0908@gmail.com>
2023-07-07 09:06:39 -0300

    Criando migrations com SQLite

--- database/migrations/2023_07_07_114151_create_produtos_table.php
+++ database/migrations/2023_07_07_114151_create_produtos_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateContatosTable extends Migration
+class CreateProdutosTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class CreateContatosTable extends Migration
      */
     public function up()
     {
-        Schema::create('contatos', function (Blueprint $table) {
+        Schema::create('produtos', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('nome');
-            $table->string('tel');
-            $table->string('email');
+            $table->string('titulo');
+            $table->string('descricao');
+            $table->string('valor',5,2);
             $table->timestamps();
         });
     }
@@ -29,6 +29,6 @@ class CreateContatosTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('contatos');
+        Schema::dropIfExists('produtos');
     }
 }
